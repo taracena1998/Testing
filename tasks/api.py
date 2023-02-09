72e2274003be56946637f9a4707e688741634537
Taras <taraslavrysh1998@gmail.com>
2023-02-09 20:07:36 +0100

    Add new files to the repo.

---
+++ tasks/api.py
@@ -0,0 +1,10 @@
+def add(task):
+def get(task_id):
+def list_tasks(owner=None):
+def count():
+def update (task_id, task):
+def delete(task_id):
+def delete_all():
+def unique_id():
+def start_tasks_db(db_path, db_type):
+def stop_tasks_db():
---
+++ tests/unit/tasks_m.py
@@ -0,0 +1,5 @@
+import  pytest
+from collections import namedtuple
+
+Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
+Task.__new__.__defaults__=(None, None, False, None)
